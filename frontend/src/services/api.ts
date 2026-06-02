afd050595ef3b8ca88d52377aa9f3ead23935ea8
sunidhi shinde <sunidhishinde28@gmail.com>
2026-06-02 12:46:44 +0530

    Updated frontend API URL for production

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,27 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-
-  baseURL: "http://localhost:8080"
+  baseURL: "https://rbac-auth-system-u5m9.onrender.com"
 });
 
 api.interceptors.request.use(
-
   (config) => {
-
-    const token = localStorage.getItem(
-
-      "token"
-    );
+    const token = localStorage.getItem("token");
 
     if (token) {
-
-      config.headers.Authorization =
-
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
 );
 
